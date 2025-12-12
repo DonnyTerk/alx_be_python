749c54896a1389de5b978c7a1ff73d7021d9ffbb
DonnyTerk <ui.uxter@gmail.com>
2025-12-12 16:03:55 +0100

    Add polymorphism_demo.py

--- oop/main.py
+++ oop/main.py
@@ -1,13 +1,7 @@
-from library_system import Book, EBook, PrintBook, Library
+from polymorphism_demo import Rectangle, Circle
 
-library = Library()
+r = Rectangle(5, 3)
+c = Circle(4)
 
-b1 = Book("Pride and Prejudice", "Jane Austen")
-b2 = EBook("Snow Crash", "Neal Stephenson", 500)
-b3 = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-library.add_book(b1)
-library.add_book(b2)
-library.add_book(b3)
-
-library.list_books()
+print(r.area())
+print(c.area())
